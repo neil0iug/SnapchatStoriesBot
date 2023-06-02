e9ede41b0ae5aad9781f9a16483cf503a15423dc
neil0iug <119874047+neil0iug@users.noreply.github.com>
2023-06-02 23:08:22 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,11 +10,11 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
+    API_ID = int(os.environ.get("API_ID", "29390803"))
+    API_HASH = os.environ.get("API_HASH", "4227e730acf4483f59ad2799c2bd063f")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5993962044:AAHF2H0JETQxwbKeb-ugcJyCAJr2FSQRfUo")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Snapchatstoriesdownloaderbot")
+    AUTH_USER = int(os.environ.get("AUTH_USER", 1383315175))
 
 
 Config = ENV_VARS
